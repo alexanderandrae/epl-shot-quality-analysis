c191f336d884a08e74983598b177903923a29a72
alexanderandrae <alexander.andrae@outlook.com>
2026-04-24 12:53:11 +0100

    Create 04_analysis_queries.sql

---
+++ sql/04_analysis_queries.sql
@@ -0,0 +1,8 @@
+-- 04_analysis_queries.sql
+-- Purpose: answer the main analysis questions for the EPL Shot Quality Analysis project.
+
+-- Analysis queries will include:
+-- 1. Team attacking profile
+-- 2. Team defensive profile
+-- 3. Shot quality comparison
+-- 4. Player shooting contribution
