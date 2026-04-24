417b91b9ffcfdbdd02c8201debf4927950ac9c45
alexanderandrae <alexander.andrae@outlook.com>
2026-04-24 12:52:33 +0100

    Create 03_derived_tables.sql

---
+++ sql/03_derived_tables.sql
@@ -0,0 +1,4 @@
+-- 03_derived_tables.sql
+-- Purpose: create derived analytical tables such as team_match_stats and team_season_stats.
+
+-- Derived table logic will be added after cleaned tables are available.
